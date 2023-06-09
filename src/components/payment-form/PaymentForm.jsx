81d665ea6b7a478a915baeb777b92ef9942c0adf
Ali Adeku <aliadeku.aam@gmail.com>
2023-06-09 22:40:24 +0100

    Add env variables

--- src/components/payment-form/PaymentForm.jsx
+++ src/components/payment-form/PaymentForm.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { CardElement } from "@stripe/react-stripe-js";
 import './payment.style.scss';
 import PaystackPop from '@paystack/inline-js'
@@ -6,13 +7,13 @@ import Button from "../button/Button";
 
 const PaymentForm = ({show,setShow,total}) => {
     const user = useSelector((state)=>state.user.currentUser)
-
+   console.log(process.env.REACT_APP_PAYSTACK_KEY,"adeku")
 
     const payWithPaystack = (e) =>{
         e.preventDefault();
     const paystack = new PaystackPop()
     paystack.newTransaction({
-        key : 'pk_test_ff6fcb1470a68d6059152e10f9c0d24d0e7dc97a',
+        key : process.env.REACT_APP_PAYSTACK_KEY,
         amount : total,
         email : user.email,
     })
